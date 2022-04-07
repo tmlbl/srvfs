e0c9f56185c82f011062e4b5ffd79dab43ec5d4c
Tim Lebel <tim.lebel@gmail.com>
2022-04-07 09:24:31 -0400

    Update deps and add structopt

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
+use structopt::StructOpt;
+
 mod vfs;
 mod srvfs;
 
+#[derive(StructOpt, Debug)]
+#[structopt(name = "srvfs")]
+struct Options {
+    #[structopt(long = "nats", default_value = "127.0.0.1")]
+    nats_addr: String,
+}
+
 fn main() {
-    let nc = nats::connect("127.0.0.1").unwrap();
+    let opts = Options::from_args();
+
+    println!("Connecting to NATS at {}...", opts.nats_addr);
+    let nc = nats::connect(&opts.nats_addr).unwrap();
     srvfs::mount("/tmp/srvfs", nc);
 }
